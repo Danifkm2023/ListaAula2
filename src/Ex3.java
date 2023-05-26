ed217f49ddf924886f31cf04a71ede4faa8ba9bd
Danifkm2023 <daniel.gustavo@zup.com.br>
2023-05-26 07:58:10 -0300

    Correção do erro de escopo do if, reaproveitamento de codigo

--- src/Ex3.java
+++ src/Ex3.java
@@ -4,55 +4,56 @@ import java.util.Scanner;
 public class Ex3 {
 
     public static class Exercicio3 {
-        private static String InputStream;
 
         public static void main(String[] args) {
 
             Scanner entrada = new Scanner(System.in);
 
+
             System.out.println("Responda as seguintes perguntas sobre o crime com (S/N):");
-            System.out.println("Telefonou para a vítima?");
-            String resposta1 = entrada.nextLine();
-            System.out.println("Esteve no local do crime?");
-            String resposta2 = entrada.nextLine();
-            System.out.println("Mora perto da vítima?");
-            String resposta3 = entrada.nextLine();
-            System.out.println("Devia para a vítima?");
-            String resposta4 = entrada.nextLine();
-            System.out.println("Já trabalhou com a vítima?");
-            String resposta5 = entrada.nextLine();
+
+            String resposta1 = geraMenu("Telefonou para a vítima?",entrada);
+
+            String resposta2 = geraMenu("Esteve no local do crime?",entrada);
+
+            String resposta3 = geraMenu("Mora perto da vítima?",entrada);
+
+            String resposta4 = geraMenu("Devia para a vítima?",entrada);
+
+            String resposta5 = geraMenu("Já trabalhou com a vítima?",entrada);
 
             int quantidadeDeSim = 0;
             int quantidadeDeNao = 0;
 
-            if(resposta1.equalsIgnoreCase(InputStream));{
+            if(resposta1.equalsIgnoreCase("s")){
                 quantidadeDeSim++;
             }
-            if(resposta2.equalsIgnoreCase(InputStream));{
+            if(resposta2.equalsIgnoreCase("s")){
                 quantidadeDeSim++;
             }
-            if(resposta3.equalsIgnoreCase(InputStream));{
+            if(resposta3.equalsIgnoreCase("s"))
+            {
                 quantidadeDeSim++;
             }
-            if(resposta4.equalsIgnoreCase(InputStream));{
+            if(resposta4.equalsIgnoreCase("s")){
                 quantidadeDeSim++;
             }
-            if(resposta5.equalsIgnoreCase(InputStream));{
+            if(resposta5.equalsIgnoreCase("s")){
                 quantidadeDeSim++;
             }
-            if (resposta1.equalsIgnoreCase(InputStream));{
+            if (resposta1.equalsIgnoreCase("n")){
                 quantidadeDeNao++;
             }
-            if (resposta2.equalsIgnoreCase(InputStream));{
+            if (resposta2.equalsIgnoreCase("n")){
                 quantidadeDeNao++;
             }
-            if (resposta3.equalsIgnoreCase(InputStream));{
+            if (resposta3.equalsIgnoreCase("n")){
                 quantidadeDeNao++;
             }
-            if (resposta4.equalsIgnoreCase(InputStream));{
+            if (resposta4.equalsIgnoreCase("n")){
                 quantidadeDeNao++;
             }
-            if (resposta5.equalsIgnoreCase(InputStream));{
+            if (resposta5.equalsIgnoreCase("n")){
                 quantidadeDeNao++;
             }
             System.out.println("Essa é a quantidade de não:" +quantidadeDeNao);
@@ -64,17 +65,23 @@ public class Ex3 {
                 System.out.println("Ele(a) é Cúmplice");
             } else if (quantidadeDeSim == 5) {
                 System.out.println("Ele(a) é o(a) Assasino(a)");{
-            if (quantidadeDeNao >= 5){
-                System.out.println("Ele é inocente");
-            }
+                    if (quantidadeDeNao >= 5){
+                        System.out.println("Ele é inocente");
+                    }
 
                 }
             }
 
 
 
-                }
-            }
         }
+    }
+
+
+    private static String geraMenu(String mensagem, Scanner entrada){
+        System.out.println(mensagem);
+        return entrada.nextLine();
+    }
+}
 
 
